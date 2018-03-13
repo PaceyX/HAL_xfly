55ef40449a05079094e316f1a8283c69cf5067ba
Pacey <px@imscv.com>
2018-03-13 18:28:21 +0800

    超声波模块开调

--- BSP/Src/ultrasonic.c
+++ BSP/Src/ultrasonic.c
@@ -3,7 +3,7 @@
 * @file    ultrasonic.c
 * @author  Pacey
 * @date    2018/03/13
-* @brief   us015 ultrasonic distance module.
+* @brief   US015 ultrasonic distance module.
 * @attention Copyright (C) X-FLY team.
 ******************************************************************************
 */
@@ -17,4 +17,27 @@
 void BSP_GPIO_SonicInit(void)
 {
     
+	
+}
+
+/**
+*  @brief  Send start info.
+*  @param  none.
+*  @retval none.
+*  @note   send 10us high level.
+*/
+void SendStartInfo(void)
+{
+	
+}
+
+/**
+*  @brief  Get high level time.
+*  @param  none.
+*  @retval the high level time(us).
+*  @note   Be called in timer interrupt per 10us.
+*/
+uint16_t ultraGetOutputCallBack(void)
+{
+	
 }
