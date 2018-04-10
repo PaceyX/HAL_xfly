3a9efe19e3794eaf8f59396848b11622718eafc2
Pacey <peixujkl@gmail.com>
2018-04-10 23:40:27 +0800

    debuging...

--- BSP/Src/ms5611.c
+++ BSP/Src/ms5611.c
@@ -30,7 +30,15 @@ static void MS_CS_Disable(void)
 */
 uint8_t MS5611_WriteReg(uint8_t reg)
 {
-	return HAL_SPI_Transmit(&hspi1, &reg,1,0xFFFFFF);
+	uint8_t reg_value;
+	
+	MS_CS_Enable();
+
+	HAL_SPI_Transmit(&hspi1, &reg, 1, 0xFFFF);
+	HAL_SPI_Receive(&hspi1, &reg_value, 1, 0xFFFF);
+	MS_CS_Disable();
+	
+	return reg_value;
 }
 
 /**
@@ -38,39 +46,42 @@ uint8_t MS5611_WriteReg(uint8_t reg)
 *	@param	reg : the register.
 *	@retval	data.
 */
-uint8_t MS5611_ReadReg(uint8_t reg)
-{
-	uint8_t reg_value;
-	HAL_SPI_Transmit(&hspi1, &reg, 1, 0xFFFF);
-	return HAL_SPI_Receive(&hspi1, &reg_value, 1, 0xFFFF);; 
-}
+//uint8_t MS5611_ReadReg(uint8_t reg)
+//{
+//	uint8_t reg_value;
+//	HAL_SPI_Transmit(&hspi1, &reg, 1, 0xFFFF);
+//	return HAL_SPI_Receive(&hspi1, &reg_value, 1, 0xFFFF); 
+//}
 
-/**
-*	@brief	Reset target.
-*/
-void MS5611_Reset(void)
+
+void MS5611_MultiRead(uint8_t Reg, uint8_t *Val, uint8_t cnt)
 {
+	uint8_t i;
+	
 	MS_CS_Enable();
-	MS5611_WriteReg(MS5611_CMD_RESET);		/* MS5611 Reset. */
+	MS5611_WriteReg(Reg);
+	
+	for(i=0;i<cnt;i++)
+	{
+		HAL_SPI_Transmit(&hspi1, 0x00, 1, 0xFFFF);
+		HAL_SPI_Receive(&hspi1, Val, 1, 0xFFFF); 
+		Val++;
+	}
 	MS_CS_Disable();
 }
+
 
 /**
 *	@brief	Read prom.
 */
 void MS5611_ReadProm(void)
 {
-	uint8_t rxbuf[2] = {0,0};
+	uint8_t buf[2] = {0,0};
 	
 	for(int i = 0; i < 8; i++)
 	{
-		MS_CS_Enable();
-		MS5611_WriteReg(MS5611_CMD_PROM_BASE + i * 2);
-		rxbuf[0] = MS5611_ReadReg(0x00);
-		rxbuf[1] = MS5611_ReadReg(0x00);
-		MS5611_prom[i] = rxbuf[0] << 8 | rxbuf[1];
-		HAL_Delay(1);
-		MS_CS_Disable();		
+		MS5611_MultiRead((MS5611_CMD_PROM_BASE + i * 2), buf, 2);
+		MS5611_prom[i] = (uint16_t)buf[0] << 8 | buf[1];	
 	}
 }
 
@@ -79,7 +90,7 @@ void MS5611_ReadProm(void)
 */
 void MS5611_Init(void)
 {
-	MS5611_Reset();
+	MS5611_WriteReg(MS5611_CMD_RESET);
 	MS5611_ReadProm();
 }
 
